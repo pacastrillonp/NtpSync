a3ac37d0da17785693e48b480c677e63faf44bc4
Pablo Castrillón <pablo.castrillon@tekus.co>
2023-07-28 16:40:21 -0500

    Mqtt

--- app/src/main/java/co/pacastrillonp/ntpsync/MainActivity.kt
+++ app/src/main/java/co/pacastrillonp/ntpsync/MainActivity.kt
@@ -2,6 +2,7 @@ package co.pacastrillonp.ntpsync
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -14,25 +15,38 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import co.pacastrillonp.ntpsync.ui.theme.NtpSyncTheme
+import co.pacastrillonp.ntpsync.util.MqttClientManager
 import co.pacastrillonp.ntpsync.util.getCurrentTime
 import kotlinx.coroutines.delay
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended
+import org.eclipse.paho.client.mqttv3.MqttMessage
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
 class MainActivity : ComponentActivity() {
+
+    private val brokerUrl =
+        "172.16.20.49:1883" // Reemplaza con la dirección IP del servidor MQTT
+    private val broadcastTopic = "broadcast_topic"
+
+    private lateinit var mqttClientManager: MqttClientManager
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
             NtpSyncTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -43,6 +57,46 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        mqttClientManager = MqttClientManager(brokerUrl)
+
+        // Suscribirse al canal de difusión (broadcast)
+        mqttClientManager.subscribeToTopic(broadcastTopic, object : MqttCallbackExtended {
+            override fun connectionLost(cause: Throwable?) {
+                // Lógica en caso de pérdida de conexión
+            }
+
+            override fun messageArrived(topic: String?, message: MqttMessage?) {
+                // Lógica para procesar el mensaje recibido
+                message?.let {
+                    val receivedData = String(it.payload)
+                    runOnUiThread {
+                        // Actualizar la interfaz de usuario con el mensaje recibido
+                        // Por ejemplo, muestra el mensaje en un TextView
+                        Log.d("🛸", "receivedData: $receivedData")
+                    }
+                }
+            }
+
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                // Lógica en caso de entrega completa
+            }
+
+            override fun connectComplete(reconnect: Boolean, serverURI: String?) {
+                // Lógica en caso de conexión completa
+            }
+        })
+
+        // Publicar un mensaje en el canal de difusión
+        val message = "Hola a todos los dispositivos!"
+        mqttClientManager.publishMessage(broadcastTopic, message)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Desconectar el cliente MQTT al destruir la actividad
+        mqttClientManager.disconnect()
     }
 }
 
